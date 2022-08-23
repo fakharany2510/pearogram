97c0576dc1f590c4b81574da630d93ce93017703
fakharany2510 <fakharany00@gmail.com>
2022-08-23 10:09:19 +0200

    a

---
+++ lib/presentation/constants/color_manager.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class AppColor{
+  static Color primaryColor = HexColor('#89A71A');
+  static Color lightPrimaryColor = HexColor('#A5C23984');
+  static Color white = HexColor('#FFFFFF');
+  static Color headLineFontColor = HexColor('#000000FC');
+  static Color hintFontColor = HexColor('#0E0B0BBC');
+  static Color smoothIndicatorColor = HexColor('#A5C239');
+  static Color disabledSmoothIndicatorColor = HexColor('#0E0B0B24');
+  static Color disabledButtonColor = HexColor('#6C757D');
+  static Color activeNowColor = HexColor('#4CD137');
+  static Color detailsButtonColor = HexColor('#0000000A');
+  static Color forgetPasswordColor = HexColor('#33426BFC');
+}
